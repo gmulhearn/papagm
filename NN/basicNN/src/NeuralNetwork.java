901ac9867faa240e8c43b605b455c4be9ca493ce
gmulhearn <g.mulhearn@uqconnect.edu.au>
2019-11-08 00:20:40 +1000

    slow progress on NN

--- NN/basicNN/src/NeuralNetwork.java
+++ NN/basicNN/src/NeuralNetwork.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class NeuralNetwork {
+    private List<Layer> layers;
+
+    public NeuralNetwork(int[] layerSizes) {
+        this.layers = new ArrayList<>();
+
+        for (int i = 0; i < layerSizes.length; i++) {
+            Layer layer;
+            if (i == 0) {
+                layer = new Layer(layerSizes[i], null);
+            } else {
+                layer = new Layer(layerSizes[i], layers.get(i - 1));
+            }
+
+            this.layers.add(layer);
+        }
+    }
+
+    public List<Layer> getLayers() {
+        return this.layers;
+    }
+
+    public void randomizeAll() {
+        for (Layer layer : this.layers) {
+            layer.randomiseNeurons();
+        }
+    }
+
+    public static void main(String[] args) {
+        int layers[] = {3, 2, 1};
+        NeuralNetwork nn = new NeuralNetwork(layers);
+        System.out.println(nn.getLayers());
+    }
 }
---
+++ NN/basicNN/src/Neuron.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Neuron {
+    private double activation;
+    private HashMap<Neuron, Double> parentNeurons;
+    private double bias;
+    public int ID;
+
+    public Neuron(int ID) {
+        this.parentNeurons = new HashMap<>();
+        this.ID = ID;
+    }
+
+
+    public void setWeight(Neuron neuron, double weight) {
+        this.parentNeurons.put(neuron, weight);
+    }
+
+    public void setBias(double bias) {
+        this.bias = bias;
+    }
+
+    public double getActivation() {
+        return this.activation;
+    }
+
+    public void setActivation(double activation) {
+        this.activation = activation;
+    }
+
+    public double calculateActivation() {
+        double sum = 0;
+        for (Map.Entry<Neuron, Double> entry : parentNeurons.entrySet()) {
+            sum += entry.getKey().getActivation() * entry.getValue();
+        }
+
+        sum -= this.bias;
+
+        return sigmoid(sum);
+    }
+
+    public static double sigmoid(double x) {
+        return (1 / (1 + Math.pow(Math.E, (-1 * x))));
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+        for (Map.Entry<Neuron, Double> entry : parentNeurons.entrySet()) {
+            str += String.format("(" + entry.getKey().ID + "," +
+                    entry.getValue() + "), ");
+        }
+        return (String.format("\nID: " + ID + " activation: " + activation +
+                " bias: " + bias + " weights: " + str));
+    }
+}
